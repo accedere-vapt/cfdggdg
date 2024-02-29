cbdaf792631f22f762c3d826c498c2fc38840421
accedere-vapt <161581019+accedere-vapt@users.noreply.github.com>
2024-02-29 12:54:46 +0530

    Delete Common directory

--- Common/_vti_cnf/pfIncEntityGLFunctions.js
+++
@@ -1,4 +0,0 @@
-vti_encoding:SR|utf8-nl
-vti_timelastmodified:TR|14 Feb 2002 22:08:00 -0000
-vti_extenderversion:SR|4.0.2.3406
-vti_backlinkinfo:VX|pf/functions/payment/pfaddpayment.asp pf/reports/pfrptperldgrsumm.asp pf/functions/tdsremittance/pfaddtdsremittancesubmission.asp pf/functions/tdsremittance/pfmodifytdsremittancesubmission.asp pf/functions/budget/pflookupbudget.asp pf/masters/entitygl/pfaddentityglmst.asp pf/reports/pfrptgltxn.asp pf/functions/budget/pfintbudget.asp pf/functions/budget/allocation/pfnewbudgetalloc.asp pf/functions/gltransaction/pfaddtxn.asp pf/functions/payment/pfmodifypymt.asp pf/functions/pettycash/pfaddpettycashposting.asp pf/functions/receipt/pfaddrcpt.asp pf/functions/receipt/pfmodifyrcpt.asp pf/functions/reconciliation/pfintreconciliation.asp pf/reports/pfrptpymtbook.asp pf/functions/gltransaction/pfaddtxnnew.asp pf/reports/pfrptpayinslip.asp pf/masters/entitygl/pfintentityglmst.asp pf/reports/pfrptcashbk.asp pf/reports/pfrptperldgrdtl.asp
